35d4be5d6f4fb66227575997baca271d6a0f6b78
danawnl-ing <guerragiraldodaniel@gmail.com>
2025-04-18 16:09:13 -0500

    Implementing the 'Login' into the 'App.js' without affecting any other files. ✨Some visual things of the 'Login' view it's still in process.🚧

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        { <Route path="/login" element={<Login />} /> }
         {/* <Route path="/register" element={<Register />} /> */}
       </Routes>
     </Router>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import "../styles/login.css";
+import styles from "../styles/login.module.css";
 
-const Login = () => {
-    return (
-        <div class="container">
-            <div class="form-box">
-                <h2>Iniciar Sesion</h2>
-                <p>¿No tienes una cuenta? <a class="crear-cuenta-link" href="../">Crear cuenta</a></p>
-                <form method="POST" action="/login">
-                    <input type="text" placeholder="Usuario" required/>
-                    <input type="password" placeholder="Contraseña" required />
-                    <div class="extra options">
-                        <label><input type="checkbox" style={{marginRight: "5px"}}/>Recuerdame</label>
-                        <a href="#">¿Olvidaste tu contraseña?</a>
-                    </div>
-                    <button type="submit">Iniciar sesión</button>
-                </form>
-            </div>
-        </div>
-    )
+const LoginForm = () => {
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.formBox}>
+        <h2 className={styles.title}>Iniciar sesión</h2>
+        <p className={styles.paragraph}>
+          ¿No tienes una cuenta?{" "}
+          <a className={styles.crearCuentaLink} href="/register">
+            Crear cuenta
+          </a>
+        </p>
+        <form className={styles.form}>
+          <input className={styles.input} type="text" placeholder="Usuario" required />
+          <input className={styles.input} type="password" placeholder="Contraseña" required />
+          <div className={styles.extraOptions}>
+            <label>
+              <input type="checkbox" style={{ marginRight: "5px" }} /> Recuérdame
+            </label>
+            <a href="#">¿Olvidaste tu contraseña?</a>
+          </div>
+          <button className={styles.button} type="submit">
+            Iniciar sesión
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginForm;
